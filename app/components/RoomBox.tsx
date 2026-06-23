b269bdd22dec44f3b407e643d04b1e0a147a7d05
Hasan Alasker <hasanalasker2004@gmail.com>
2026-06-23 21:44:34 +0300

    added public rooms

--- app/components/RoomBox.tsx
+++ app/components/RoomBox.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import Button from "./Button";
 
-export default function RoomBox({ id }: { id: string }) {
+export default function RoomBox({ name, id }: { name: string; id: string }) {
   return (
     <div className="flex justify-between items-center">
-      <h2 className="truncate">{id}</h2>
+      <h2 className="truncate">{name}</h2>
       <Link href={`/room/${id}`}>
         <Button title="Join" style="mid" />
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -14,19 +14,20 @@ export default function Home() {
   return (
     <div className="flex flex-col space-y-15">
       <Button
-        title="Create Room"
+        title="Private Room"
         icon="plus"
         style="pri"
         onclick={createRoom}
       />
 
       <section className="border border-outline px-4 py-6 rounded-md bg-card">
-        <h1>Available rooms</h1>
+        <h1>Public rooms</h1>
         <div className="flex flex-col space-y-2 ">
-          {/* map through available rooms */}
-          <RoomBox id="vszc_233fafd" />
-          <RoomBox id="afdcvz_00043" />
-          <RoomBox id="adfczvcvz_43" />
+          <RoomBox name="Room 1" id="publicRoom1" />
+          <RoomBox name="Room 2" id="publicRoom2" />
+          <RoomBox name="Room 3" id="publicRoom3" />
+          <RoomBox name="Room 4" id="publicRoom4" />
+          <RoomBox name="Room 5" id="publicRoom5" />
         </div>
       </section>
     </div>
